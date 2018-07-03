b3b811c94fdbf336f71a32ee31855c7f7936ea35
Unknown <rauha.07@gmail.com>
2018-07-03 23:45:03 +0300

    Заполняю и печатаю массив сотрудников

--- src/Collaborators.java
+++ src/Collaborators.java
@@ -1,9 +1,9 @@
 import java.util.Calendar;
 
 public class Collaborators {
-    private static String name;
-    private static long salary;
-    private static int birthYear;
+    private String name;
+    private long salary;
+    private int birthYear;
 
     public Collaborators() {
     }
@@ -14,12 +14,11 @@ public class Collaborators {
         this.birthYear = cBirthYear;
     }
 
-
     public String getName(){
         return this.name;
     }
 
-    public long getSlary(){
+    public long getSalary(){
         return this.salary;
     }
 
@@ -33,11 +32,29 @@ public class Collaborators {
         this.salary = amount;
     }
 
+    public static void printArray(Collaborators[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.println("ФИО: " + arr[i].getName());
+            System.out.println("Зарплата: " + arr[i].getSalary());
+            System.out.println("Возраст: " + arr[i].getAge());
+            System.out.println();
+        }
+    }
 
     public static void main(String[] arg){
 
-        Collaborators[] collabArr = new Collaborators[5];
+        Collaborators c1 = new Collaborators("Иванов Сергей", 35000, 1990);
+        Collaborators c2 = new Collaborators("Лежнина Анастасия", 50000, 1975);
+        Collaborators c3 = new Collaborators("Сергеев Михаил", 45000, 1980);
+        Collaborators c4 = new Collaborators("Макарова Татьяна", 25000, 1995);
+        Collaborators c5 = new Collaborators("Петров Алексей", 55000, 1970);
+
+        Collaborators[] collabArr = {c1, c2, c3, c4, c5};
+        printArray(collabArr);
+
 
     }
+
+
 
 }
