138eb627e7e1ebbcf0638f47e187a72eb5570a9c
Unknown <rauha.07@gmail.com>
2018-07-03 23:02:37 +0300

    Добавлен каркас класса сотрудников

--- src/Collaborators.java
+++ src/Collaborators.java
@@ -1,5 +1,43 @@
-/**
- * Created by maksc on 03.07.2018.
- */
+import java.util.Calendar;
+
 public class Collaborators {
+    private static String name;
+    private static long salary;
+    private static int birthYear;
+
+    public Collaborators() {
+    }
+
+    public Collaborators(String cName, long cSalary, int cBirthYear) {
+        this.name = cName;
+        this.salary = cSalary;
+        this.birthYear = cBirthYear;
+    }
+
+
+    public String getName(){
+        return this.name;
+    }
+
+    public long getSlary(){
+        return this.salary;
+    }
+
+    public int getAge(){
+        Calendar calendar = Calendar.getInstance();
+        int currentYear = calendar.get(Calendar.YEAR);
+        return currentYear - this.birthYear;
+    }
+
+    public void setSlary(int amount){
+        this.salary = amount;
+    }
+
+
+    public static void main(String[] arg){
+
+        Collaborators[] collabArr = new Collaborators[5];
+
+    }
+
 }
